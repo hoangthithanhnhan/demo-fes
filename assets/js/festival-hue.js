9514e380b937037577fe86b0b6c38921835cc5c8
Hoàng Thị Thanh Nhàn <125141035+hoangthithanhnhan@users.noreply.github.com>
2024-06-29 10:27:17 +0700

    29/6

---
+++ assets/js/festival-hue.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#slide-img-fes').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        items:1
+    })
+})
--- assets/js/tin-tuc-hd.js
+++ assets/js/tin-tuc-hd.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $('#zoom-in').on('click',function(){
-        var currentFontSize = parseFloat($('#text p').css('font-size'));
-        var currentFontLineHeight = parseFloat($('#text p').css('line-height'));
+        var currentFontSize = parseInt($('#text p').css('font-size'));
+        var currentFontLineHeight = parseInt($('#text p').css('line-height'));
         $('#text p').css('font-size', (currentFontSize + 2) + 'px');
         $('#text p').css('line-height', (currentFontLineHeight + 2) + 'px');
     })
     $('#zoom-out').on('click',function(){
-        var currentFontSize = parseFloat($('#text p').css('font-size'));
-        var currentFontLineHeight = parseFloat($('#text p').css('line-height'));
+        var currentFontSize = parseInt($('#text p').css('font-size'));
+        var currentFontLineHeight = parseInt($('#text p').css('line-height'));
         $('#text p').css('font-size', (currentFontSize - 2) + 'px');
         $('#text p').css('line-height', (currentFontLineHeight - 2) + 'px');
     })
