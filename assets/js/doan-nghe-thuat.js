2538d47f4b6d7f5fd6a981a43751ff3b49c8cec4
Hoàng Thị Thanh Nhàn <125141035+hoangthithanhnhan@users.noreply.github.com>
2024-07-01 17:33:44 +0700

    ...

--- assets/js/doan-nghe-thuat.js
+++ assets/js/doan-nghe-thuat.js
@@ -1,3 +1,5 @@
-$(document).ready(function() {
-    
-})
+// $(document).ready(function() {
+//     $('#collapse1').on('shown.bs.collapse',function(){
+//         $('main .fes .content-left .gioi-thieu-menu nav>ul li .dropdown::after').css('transform','scaleY(-1)');
+//     })
+// })
