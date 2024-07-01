24eb678e843ace3ed8d52e57f492e08706efdc95
Hoàng Thị Thanh Nhàn <125141035+hoangthithanhnhan@users.noreply.github.com>
2024-07-01 11:45:32 +0700

    sang1/7

--- assets/js/tin-tuc-hd.js
+++ assets/js/tin-tuc-hd.js
@@ -1,19 +1,14 @@
 $(document).ready(function() {
     $('#zoom-in').on('click',function(){
-        var currentFontSize = parseInt($('#text p').css('font-size'));
-        var currentFontLineHeight = parseInt($('#text p').css('line-height'));
+        var currentFontSize = parseFloat($('#text p').css('font-size'));
         $('#text p').css('font-size', (currentFontSize + 2) + 'px');
-        $('#text p').css('line-height', (currentFontLineHeight + 2) + 'px');
     })
     $('#zoom-out').on('click',function(){
-        var currentFontSize = parseInt($('#text p').css('font-size'));
-        var currentFontLineHeight = parseInt($('#text p').css('line-height'));
+        var currentFontSize =parseFloat($('#text p').css('font-size'));
         $('#text p').css('font-size', (currentFontSize - 2) + 'px');
-        $('#text p').css('line-height', (currentFontLineHeight - 2) + 'px');
     })
     $('#reset').on('click',function(){
         $('#text p').css('font-size','16px');
-        $('#text p').css('line-height','24px');
     })
     $('#print').click(function() {
         window.print();
