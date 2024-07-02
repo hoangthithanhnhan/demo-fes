00e26f1780c39dd170b0a727aa4d2c552e71a2cf
Hoàng Thị Thanh Nhàn <125141035+hoangthithanhnhan@users.noreply.github.com>
2024-07-02 08:20:12 +0700

    2/7

--- assets/js/doan-nghe-thuat.js
+++
@@ -1,5 +0,0 @@
-// $(document).ready(function() {
-//     $('#collapse1').on('shown.bs.collapse',function(){
-//         $('main .fes .content-left .gioi-thieu-menu nav>ul li .dropdown::after').css('transform','scaleY(-1)');
-//     })
-// })
