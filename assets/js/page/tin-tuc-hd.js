15f7a89d62c76d9fedc35fbc1528dfbf6a9e1271
Hoàng Thị Thanh Nhàn <125141035+hoangthithanhnhan@users.noreply.github.com>
2024-07-26 09:53:45 +0700

    ...

--- assets/js/page/tin-tuc-hd.js
+++ assets/js/page/tin-tuc-hd.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-    $('#zoom-in').on('click',function(){
-        var currentFontSize = parseFloat($('#text p').css('font-size'));
-        $('#text p').css('font-size', (currentFontSize + 2) + 'px');
-    })
-    $('#zoom-out').on('click',function(){
-        var currentFontSize =parseFloat($('#text p').css('font-size'));
-        $('#text p').css('font-size', (currentFontSize - 2) + 'px');
-    })
-    $('#reset').on('click',function(){
-        $('#text p').css('font-size','16px');
-    })
-    $('#print').click(function() {
-        window.print();
-    });
     renderNewSideBar('bai-doc-nhieu-nhat',0,'view')
     renderContent('6F533950-0750-4D79-991C-B07A0097FA26','#content-tin-tuc-hoat-dong',1, 12);
     $(document).on("page",'#pagination', function(evt, page){
